ecabcf9ad43f5ec4ae5f5d917aeb72b1c11a8c3b
dmosc <osdavrm@gmail.com>
2023-03-04 17:51:46 -0600

    test(api): add tests for different Spacy response entities.

--- app/named_entity_client.py
+++ app/named_entity_client.py
@@ -1,7 +1,21 @@
 class NamedEntityClient:
+    LABEL_MAP = {
+        "PERSON": "Person",
+        "NORP": "Group",
+        "NOUN": "Noun",
+        "LOC": "Location",
+        "LANGUAGE": "Language",
+        "GPE": "GeoPoliticalIdentity",
+    }
+
     def __init__(self, model):
         self.model = model
 
     def get_entities(self, sentence):
         doc = self.model(sentence)
-        return doc
+        return {"ents": [{"text": ent["text"], "label": self.map_label(ent["label_"])} for ent in doc["ents"]], "html": doc["html"]}
+
+    def map_label(self, label):
+        if label in self.LABEL_MAP:
+            return self.LABEL_MAP[label]
+        return "Unknown"
--- app/test_named_entity_client.py
+++ app/test_named_entity_client.py
@@ -6,29 +6,106 @@ from app.named_entity_client import NamedEntityClient
 @pytest.fixture
 def mock_spacy_model_with_response():
     class SpacyModel:
-        def __init__(self, ents):
-            self.ents = ents
+        def __init__(self):
+            self.ents = [],
+            self.html = ""
 
         def __call__(self, sentence):
-            return {"ents": self.ents}
+            return {"ents": self.ents, "html": self.html}
 
-    return SpacyModel
+        def set_return(self, ents, html):
+            self.ents = ents
+            self.html = html
+
+    return SpacyModel()
 
 
 def test_get_entities_dictionary_from_empty_string(mock_spacy_model_with_response):
-    named_entity_client = NamedEntityClient(mock_spacy_model_with_response([]))
-    entities = named_entity_client.get_entities("")
+    mock_spacy_model_with_response.set_return([], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities("")
 
-    assert isinstance(entities, dict)
+    assert isinstance(get_entities_response, dict)
 
 
 def test_get_entities_dictionary_from_nonempty_string(mock_spacy_model_with_response):
-    named_entity_client = NamedEntityClient(
-        mock_spacy_model_with_response([{"text": 'Madison', "label_": 'NOUN'}]))
-    entities = named_entity_client.get_entities(
+    mock_spacy_model_with_response.set_return(
+        [{"text": 'Madison', "label_": 'NOUN'}], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities(
         "Madison is a city in Wisconsin")
 
-    assert isinstance(entities, dict)
-    assert "ents" in entities
-    assert entities["ents"][0]["text"] == "Madison"
-    assert entities["ents"][0]["label_"] == "NOUN"
+    assert isinstance(get_entities_response, dict)
+
+
+def test_get_entity_PERSON_serialized_as_Person(mock_spacy_model_with_response):
+    mock_spacy_model_with_response.set_return(
+        [{"text": "Laurent Fressinet", "label_": "PERSON"}], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities(
+        "Laurent Fressinet is a famous person")
+
+    assert isinstance(get_entities_response, dict)
+    assert get_entities_response["ents"] == [
+        {"text": "Laurent Fressinet", "label": "Person"}]
+
+
+def test_get_entity_NORP_serialized_as_Group(mock_spacy_model_with_response):
+    mock_spacy_model_with_response.set_return(
+        [{"text": "Latinx", "label_": "NORP"}], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities(
+        "Meet the Latinx community")
+
+    assert isinstance(get_entities_response, dict)
+    assert get_entities_response["ents"] == [
+        {"text": "Latinx", "label": "Group"}]
+
+
+def test_get_entity_LOC_serialized_as_Location(mock_spacy_model_with_response):
+    mock_spacy_model_with_response.set_return(
+        [{"text": "ocean", "label_": "LOC"}], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities(
+        "the ocean")
+
+    assert isinstance(get_entities_response, dict)
+    assert get_entities_response["ents"] == [
+        {"text": "ocean", "label": "Location"}]
+
+
+def test_get_entity_LANGUAGE_serialized_as_Language(mock_spacy_model_with_response):
+    mock_spacy_model_with_response.set_return(
+        [{"text": "ASL", "label_": "LANGUAGE"}], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities(
+        "the ocean")
+
+    assert isinstance(get_entities_response, dict)
+    assert get_entities_response["ents"] == [
+        {"text": "ASL", "label": "Language"}]
+
+
+def test_get_entity_GPE_serialized_as_GeoPoliticalIdentity(mock_spacy_model_with_response):
+    mock_spacy_model_with_response.set_return(
+        [{"text": "European Union", "label_": "GPE"}], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities(
+        "the ocean")
+
+    assert isinstance(get_entities_response, dict)
+    assert get_entities_response["ents"] == [
+        {"text": "European Union", "label": "GeoPoliticalIdentity"}]
+
+
+def test_get_multiple_entities(mock_spacy_model_with_response):
+    mock_spacy_model_with_response.set_return(
+        [{"text": "Laurent Fressinet", "label_": "PERSON"}, {"text": "European Union", "label_": "GPE"}], "")
+    named_entity_client = NamedEntityClient(mock_spacy_model_with_response)
+    get_entities_response = named_entity_client.get_entities(
+        "Laurent Fressinet is from the European Union")
+
+    assert isinstance(get_entities_response, dict)
+    assert len(get_entities_response["ents"]) == 2
+    assert get_entities_response["ents"] == [
+        {"text": "Laurent Fressinet", "label": "Person"}, {"text": "European Union", "label": "GeoPoliticalIdentity"}]
